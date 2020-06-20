03f5087e23e109b425f32c011f3a2c51fd642746
Vizzyy <barnabas.vizy@gmail.com>
2020-06-19 22:22:45 -0400

    Remove Emailing

--- generategif.py
+++ generategif.py
@@ -1,17 +1,14 @@
+import datetime
+import glob
+import logging
 import os
 import sys
 import time
-import glob
-import smtplib
-import datetime
-import logging
+
 import mysql.connector
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 from systemd import journal
-from properties import getAddr, getEmailPass, getDbPass, getLogDir, getDbHost, getDepth, getSleepPeriod
+
+from properties import getDbPass, getLogDir, getDbHost, getDepth, getSleepPeriod
 
 scriptDir = getLogDir()
 depth = getDepth()
@@ -39,18 +36,11 @@ print("\n\nStarting up program...")
 logger.info("\n\nStarting up program...")
 while True:
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(getAddr(), getEmailPass())
         db = mysql.connector.connect(user='motion', password=getDbPass(), host=getDbHost(), port=9004,
                                      database='motion')
         cursor = db.cursor()
     except Exception as e:
-        try:
-            server.quit()
-        except Exception as b:
-            logger.info('{}'.format(b))
-        logger.info('{}'.format(e))
+        logger.info(f'{e}')
         time.sleep(5)
         continue
 
@@ -75,32 +65,30 @@ while True:
         # Calculate # of events
         for line in fullList:
             eventNumber = int(line.split('/')[depth].split('-')[0])  # isolate just the first 2 digits of filename
-
             if previousEvent != eventNumber:
                 previousEvent = eventNumber
                 eventCount = eventCount + 1
-
             try:
                 events[eventCount - 1].append(line)
             except Exception as e:
                 events.append([line])
 
         for event in range(eventCount):  # iterate through events, event = integer
-            logger.info('Creating directory: event{}'.format(event))
-            print('Creating directory: event{}'.format(event))
-            os.system('mkdir {}event{}'.format(scriptDir, event))  # make a temp directory for each event
+            logger.info(f'Creating directory: event{event}')
+            print(f'Creating directory: event{event}')
+            os.system(f'mkdir {scriptDir}event{event}')  # make a temp directory for each event
 
             for file in events[event]:  # move files into appropriate directory - file = absolute path to file
-                os.system('mv {} {}event{}/'.format(file, scriptDir, event))
+                os.system(f'mv {file} {scriptDir}event{event}/')
 
             gifName = 'event{}.gif'.format(event)
             # convert -loop 0 -layers optimize -resize 400 *.jpg output2.gif
             logger.info('   Generating gif: {}'.format(gifName))
             print('Generating gif: {}'.format(gifName))
-            os.system('convert -loop 0 -layers optimize -resize 400 {}event{}/*.jpg {}{}'.format(scriptDir, event, scriptDir, gifName))
+            os.system(f'convert -loop 0 -layers optimize -resize 400 {scriptDir}event{event}/*.jpg {scriptDir}{gifName}')
             logger.info('   Gif complete.')
             print('Gif complete.')
-            os.system('rm -R {}event{}'.format(scriptDir, event))
+            os.system(f'rm -rf {scriptDir}event{event}')
             logger.info('   Removed temp directory.')
             print('Removed temp directory.')
         try:
@@ -113,42 +101,24 @@ while True:
 
         if len(gifList) > 0:
             timeStamp = str(datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))
-            logger.info('Result directory: event{}'.format(timeStamp))
-            print('Result directory: event{}'.format(timeStamp))
-            os.system('mkdir {}event{}'.format(scriptDir, timeStamp))  # make a directory for this cycle
+            logger.info(f'Result directory: event{timeStamp}')
+            print(f'Result directory: event{timeStamp}')
+            os.system(f'mkdir {scriptDir}event{timeStamp}')  # make a directory for this cycle
             gifCounter = 0
             for file in gifList:
-                # Attach image to email
-                attachment = open(file, "rb")  # file is absolute path
-                msg = MIMEMultipart()
-                msg.attach(MIMEText("Motion Event: \n", 'plain'))
-                msg['From'] = getAddr()
-                msg['To'] = getAddr()
-                msg['Subject'] = "Motion Triggered"
-                part = MIMEBase('application', 'octet-stream')
-                part.set_payload(attachment.read())
-                encoders.encode_base64(part)
-                part.add_header('Content-Disposition', "attachment; filename= %s" % str('event' + str(gifCounter) + '.gif'))
-                msg.attach(part)
                 try:
-                    server.sendmail(getAddr(), getAddr(),
-                                    msg.as_string())  # Send mail -- Can be too large so must catch exception
-                    logger.info('Successfully emailed event #{}.'.format(str(gifCounter)))
-                    print('Successfully emailed event #{}.'.format(str(gifCounter)))
                     blob_value = open(file, 'rb').read()
                     sql = 'INSERT INTO images(Time, Image) VALUES(%s, %s)'
                     args = (str(timeStamp + '-' + file.split('/')[depth].split('.gif')[0]), blob_value)
                     cursor.execute(sql, args)
                     db.commit()
-                    logger.info('Successfully inserted event #{} into database.'.format(str(gifCounter)))
-                    print('Successfully inserted event #{} into database.'.format(str(gifCounter)))
+                    logger.info(f'Successfully inserted event #{str(gifCounter)} into database.')
+                    print(f'Successfully inserted event #{str(gifCounter)} into database.')
                 except Exception as e:
                     logger.info('{}'.format(e))
-
-                os.system('mv {} {}event{}/'.format(file, scriptDir, timeStamp))  # finally move into storage
+                os.system(f'mv {file} {scriptDir}event{timeStamp}/')  # finally move into storage
                 gifCounter = gifCounter + 1
 
     # logger.info('Sleeping for five minutes...')
-    print('Sleeping for {} minutes...'.format(sleepPeriod))
-    server.quit()
+    print(f'Sleeping for {sleepPeriod} minutes...')
     time.sleep(sleepPeriod * 60)  # sleep five minutes
